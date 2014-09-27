88105ccedf69173ea28544da6ae73f825fd94afe
Josh <Josh.Hanke@gmail.com>
2014-09-27 13:47:56 -0500

    Added abstract Tile class

---
+++ Formations/TileAbstract.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Formations
+{
+    abstract class TileAbstract
+    {
+        abstract void update()
+        {
+
+        }
+
+        abstract void draw()
+        {
+
+        }
+    }
+}
