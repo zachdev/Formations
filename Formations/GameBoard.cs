311b676b5ce1af7676ecd91bfb245884935bb1da
Josh <Josh.Hanke@gmail.com>
2014-09-27 14:27:19 -0500

    Added GameBoard and PlayerBoard classes

---
+++ Formations/GameBoard.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Formations
+{
+    class GameBoard
+    {
+        private PlayerBoard hostBoard;
+        private PlayerBoard guestBoard;
+
+        public GameBoard()
+        {
+
+        }
+
+        public void init(PlayerBoard hostBoard, PlayerBoard guestBoard)
+        {
+            this.hostBoard = hostBoard;
+            this.guestBoard = guestBoard;
+
+        }
+
+        public void update()
+        {
+
+        }
+
+        public void draw()
+        {
+
+        }
+    }
+}
--- Formations/UnitBasic.cs
+++ Formations/UnitBasic.cs
@@ -9,6 +9,7 @@ namespace Formations
     {
         public override void update()
         {
+
         }
     }
 }
