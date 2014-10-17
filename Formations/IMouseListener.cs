12f312a156614b18d8048902280e748949b11260
Josh <Josh.Hanke@gmail.com>
2014-10-17 13:24:40 -0500

    Added mouseListener back in

---
+++ Formations/IMouseListener.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Formations
+{
+    interface IMouseListener
+    {
+        void mousePressed(MouseState mouseState);
+
+        void mouseReleased(MouseState mouseState);
+
+        void mouseMoved(MouseState mouseState);
+
+        void mouseDragged(MouseState mouseState);
+    }
+}
