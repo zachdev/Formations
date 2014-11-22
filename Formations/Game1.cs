11336183cdfbd82a6d43ee340858dbf3666a90ea
meetmagdalene <meetmagdalene@gmail.com>
2014-11-22 15:02:49 -0600

    added skins folder to Content

--- Formations/Game1.cs
+++ Formations/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+// using Microsoft.Xna.Framework.GamerServices.Gamer;
 using System;
 using System.Collections.Generic;
 using TomShane.Neoforce.Controls;
