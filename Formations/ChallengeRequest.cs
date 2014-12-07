6f43e2d7f910976715139df78e5c8afadc3c024d
Josh Hanke <josh.hanke@gmail.com>
2014-12-07 12:39:57 -0600

    added challengeRequest

---
+++ Formations/ChallengeRequest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Formations
+{
+    [Serializable]
+    public class ChallengeRequest
+    {
+        private Person _sender;
+        private Person _reciever;
+        public Person Sender
+        {
+            get { return _sender; }
+            private set { _sender = value; }
+        }
+        public Person Reciever
+        {
+            get { return _reciever; }
+            private set { _reciever = value; }
+        }
+        public ChallengeRequest(Person sender, Person reciever)
+        {
+            this.Sender = sender;
+            this.Reciever = reciever;
+        }
+    }
+}
