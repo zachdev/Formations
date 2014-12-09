55fb28e333aae62087d1dac850bf01099849daa4
Josh Hanke <josh.hanke@gmail.com>
2014-12-08 18:54:04 -0600

    IGame

---
+++ Formations/IGame.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TomShane.Neoforce.Controls;
+
+namespace Formations
+{
+    public interface IGame : IUpdateDraw, IMouseListener 
+    {
+        void init(Manager uiManager, GraphicsDevice graphicsDevice, string gameName, bool isHost);
+        void update();
+        void draw(SpriteBatch spriteBatch);
+
+    }
+}
