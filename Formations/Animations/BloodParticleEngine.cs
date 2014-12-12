8b1c4143bb2e550176354fd6693e68006cc568fb
Zach Graham <zach@zachdev.com>
2014-12-12 12:08:53 -0600

    Add blood & healing particle engines

---
+++ Formations/Animations/BloodParticleEngine.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Runtime.Serialization;
+
+namespace Formations
+{
+    [Serializable]
+    public class BloodParticleEngine
+    {
+        private Random random;
+
+        private Vector2 _emitterLocation;
+        public Vector2 EmitterLocation { get { return _emitterLocation; } set { _emitterLocation = value; } }
+        private List<Particle> particles;
+
+        private List<Texture2D> textures;
+
+        public bool particlesOn { get; set; }
+        public int count;
+
+        public BloodParticleEngine(List<Texture2D> textures, Vector2 location)
+        {
+            EmitterLocation = location;
+            this.textures = textures;
+            this.particles = new List<Particle>();
+            random = new Random();
+        }
+
+        public void Update()
+        {
+            if (count > 500)
+            {
+                particlesOn = false;
+
+                count = 0;
+            }
+
+            if (particlesOn)
+            {
+                int total = 1;
+
+                for (int i = 0; i < total; i++)
+                {
+                    particles.Add(GenerateNewParticle());
+                }
+
+                count++;
+            }
+
+            for (int particle = 0; particle < particles.Count; particle++)
+            {
+                particles[particle].Update();
+                if (particles[particle].TTL <= 0)
+                {
+                    particles.RemoveAt(particle);
+                    particle--;
+                }
+            }
+        }
+
+        private Particle GenerateNewParticle()
+        {
+            Texture2D texture = textures[random.Next(textures.Count)];
+            Vector2 position = EmitterLocation;
+            Vector2 velocity = new Vector2(
+                                    1f * (float)(random.NextDouble() - 1),
+                                    1f * (float)(random.NextDouble() - 1));
+            float angle = 0;
+            float angularVelocity = 0;
+            Color color = Color.Red;
+            float size = 0.02f;
+            int ttl = 80 + random.Next(40);
+
+            return new Particle(texture, position, velocity, angle, angularVelocity, color, size, ttl);
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            for (int index = 0; index < particles.Count; index++)
+            {
+                particles[index].Draw(spriteBatch);
+            }
+        }
+    }
+}
